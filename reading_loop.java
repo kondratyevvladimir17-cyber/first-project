fc0df3d7f7fff2088cc8fb3ce0296cb7a5f0d16e
Vladimir Kondratyev <prime2boy@yandex.ru>
2026-03-06 16:56:35 +0300

    chicle

--- reading_loop.java
+++ reading_loop.java
@@ -1,7 +1,7 @@
 public class Praktikum {
   public static void main(String[] args) {
         int freeHours = 4; // Объявили переменную с количеством свободного времени в часах и присвоили ей значение
-                        
+
         checkReading(freeHours); // вызываем метод с циклом и передаём в него количество свободного времени
   }
 
